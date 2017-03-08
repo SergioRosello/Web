414515a8362c549acbf7fecd97225dd3886be488
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2017-03-08 18:31:42 +0100

    Casi acabado :/

--- Ejercicio_5/Parte_1/Ejercicio5-2.js
+++ Ejercicio_5/Parte_1/Ejercicio5-2.js
@@ -5,8 +5,24 @@ function removeCookieBanner() {
 }
 
 function doCookieOnce() {
-   removeCookieBanner();
    if (document.cookie.replace(/(?:(?:^|.*;\s*)cookie\s*\=\s*([^;]*).*$)|^.*$/, "$1") !== "true") {
-     document.cookie = "cookie=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
-  }
+      /*aqui tengo que meter una llamada a funcion que haga un display del banner*/
+      show();
+      document.cookie = "cookie=true; expires=Fri, 31 Dec 9999 23:59:59 GMT";
+   }
+   else{
+      hide();
+   }
 }
+
+
+function show() {
+   document.getElementById('cookie').style.display='block'; 
+   return false;
+} 
+function hide() {
+ if (document.cookie.indexOf("cookie=") >= 0) {
+      document.getElementById('cookie').style.display='none';
+}
+   return false;
+}  
