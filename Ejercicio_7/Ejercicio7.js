aaefbf71957563125f59ff69b76a481c48f082d1
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2017-03-15 11:16:21 +0100

    Finished Ejercicio7

--- Ejercicio_7/Ejercicio7.js
+++ Ejercicio_7/Ejercicio7.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
-    var win = $(window);
 
-    // Each time the user scrolls
     $(window).scroll(function() {
         if ($(window).scrollTop() == $(document).height() - $(window).height()) {
             $.ajax({
