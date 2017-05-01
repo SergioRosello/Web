28f381d363c1a1a0f487aafbce0c0aa09d1914a9
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2017-05-01 19:37:00 +0200

    Finished Ejercicio_13

--- Ejercicio_13/com.ejercicio13.www/src/test/java/ChromeTest.java
+++ Ejercicio_13/com.ejercicio13.www/src/test/java/ChromeTest.java
@@ -1,11 +1,13 @@
 
 
 import io.github.bonigarcia.wdm.ChromeDriverManager;
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,6 +15,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
 public class ChromeTest {
 
     private static final int TIMEOUT = 30; // seconds
@@ -34,20 +43,44 @@ public class ChromeTest {
             driver.quit();
         }
     }
+    
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {     
+                 { "sistema", "system" }, { "diccionario", "dictionary" }, {"aplicación", "application"},  { "librería", "library" }  
+           });
+    }
+
+
+    private String fInput;
+
+    private String fExpected;
+
+    public ChromeTest(String input, String expected) {
+        fInput= input;
+        fExpected= expected;
+    }
 
     @Test
-    public void testChrome(){
-        driver.get("http://www.wordreference.com/");
-        driver.findElement(By.id("si")).sendKeys("sistema");
-        driver.findElement(By.id("fSelect")).click();
-        driver.findElement(By.id("esen")).click();
-        driver.findElement(By.className("submit-button")).click();
+    public void testSistema(){
+            driver.get("http://www.wordreference.com/");
+            driver.findElement(By.id("si")).sendKeys(fInput);
+            driver.findElement(By.id("fSelect")).click();
+            driver.findElement(By.id("esen")).click();
+            driver.findElement(By.className("submit-button")).click();
 
-        WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
-        wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), "sistema"));
-        System.out.println(driver.findElements(By.className("ToWrd")).get(1).getText());
-        String text = driver.findElements(By.className("ToWrd")).get(1).getText();
-        String arr[] = text.split(" ");
-        assertEquals(arr[0], "system");
+            WebDriverWait wait = new WebDriverWait(driver, TIMEOUT);
+            wait.until(ExpectedConditions.textToBePresentInElementLocated(By.tagName("body"), fInput));
+            System.out.println(driver.findElements(By.className("ToWrd")).get(1).getText());
+            String text = driver.findElements(By.className("ToWrd")).get(1).getText();
+            String arr[] = text.split(" ");
+            boolean check = false;
+            
+            for (int i = 0; i < arr.length; i++) {
+    			if (arr[i].equals(fExpected)){
+    				check = true;
+    			}
+    		}
+            assertTrue("Spanish " + fInput + " to English " + fExpected, check);
     }
 }
