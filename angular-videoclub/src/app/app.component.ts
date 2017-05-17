dde6b0d57d8054f8be2f8aad61476d39c20f43d7
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2017-05-17 18:37:29 +0200

    Improved REST implementation

--- angular-videoclub/src/app/app.component.ts
+++ angular-videoclub/src/app/app.component.ts
@@ -1,16 +1,27 @@
 import { Component } from '@angular/core';
+// Para el servico REST
+import { Http } from '@angular/http';
 import { FilmService } from './film.service';
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html'
 })
 export class AppComponent {
+
 	private films: string[] = [];
-	constructor(private filmService: FilmService) { }
+
+	constructor(private filmService: FilmService, private http: Http) { }
+
 	search(title: string) {
-		this.films = this.filmService.getFilm(title);
+		this.films = [];
+		this.filmService.getFilms(title).subscribe(
+				films => this.films = films,
+				error => console.error(error)
+			);
 	}
+
 	list() {
 		this.films = this.filmService.getAllFilms();
 	}
+
 }
--- angular-videoclub/src/app/film.service.ts
+++ angular-videoclub/src/app/film.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/Rx';
 
 @Injectable()
 export class FilmService {
 
+	constructor(private http: Http) { }
+
 	private myFilms: string[] = ['Spring in Action', 'Java for Web Applications', 'Spring Boot Cookbook'];
+
 	getFilm(key: string) {
 		let out: string[] = [];
 		for (let film of this.myFilms) {
@@ -14,7 +19,18 @@ export class FilmService {
 		return out;
 	}
 
+	getFilms(title: string){
+		let url = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + title;
+		return this.http.get(url).map(response => this.extractTitles(response))
+
+	}
+
 	getAllFilms() {
 		return this.myFilms;
+	}
+
+	private extractTitles(response: Response) {
+		let out = response.json().items.map(film => film.volumeInfo.title);
+		return out;
 	}
 }
