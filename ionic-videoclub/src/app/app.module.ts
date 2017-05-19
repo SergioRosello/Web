aaa3da97b30793541701ae4c35555630433e8233
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2017-05-19 13:19:02 +0200

    roughly finished (needs fixing scrolling issues)

--- ionic-videoclub/src/app/app.module.ts
+++ ionic-videoclub/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { FilmService } from './film.service';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +19,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    FormsModule,
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,7 +31,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    FilmService
   ]
 })
 export class AppModule {}
