df74a1fb035a3c33865c05d79ed5f5b6eec86279
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2017-05-21 12:51:27 +0200

    Updated REST server URL

--- angular-videoclub/src/app/film.service.ts
+++ angular-videoclub/src/app/film.service.ts
@@ -8,7 +8,7 @@ export class FilmService {
 	constructor(private http: Http) { }
 
 	getFilm(title: string){
-		let url = "http://www.omdbapi.com/?t=" + title;
+		let url = "http://ec2-34-211-5-120.us-west-2.compute.amazonaws.com/?&t=" + title;
 		return this.http.get(url).map(
 				response => response.json()
 			);
